0fab25dfa6b32fe1b6fb0076301bb5818fc3db12
Edward Shin <contact@edwardsh.in>
2019-04-01 03:20:01 -0400

    Fix example and add cross compile Dockerfile

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 use rppal::i2c::I2c;
-use ssd1306::{mode::TerminalMode, Builder};
+use ssd1306::{mode::GraphicsMode, Builder, prelude::*};
+use embedded_graphics::fonts::Font6x8;
+use embedded_graphics::primitives::circle::Circle;
+use embedded_graphics::prelude::*;
 
 fn main() {
     let i2c = I2c::new().unwrap();
-    let mut disp: TerminalMode<_> = Builder::new().connect_i2c(i2c).into();
+    let mut disp: GraphicsMode<_> = Builder::new()
+        .with_size(DisplaySize::Display128x32)
+        .connect_i2c(i2c)
+        .into();
+
     disp.init().unwrap();
-    disp.clear().unwrap();
-    disp.print_char('a').unwrap();
+    disp.flush().unwrap();
+    disp.draw(
+        Font6x8::render_str("Hello world!")
+        .with_style(Style::with_stroke(1u8.into()))
+        .into_iter()
+    );
+    disp.draw(
+        Circle::new(Coord::new(64, 15), 5)
+        .with_style(Style::with_stroke(1u8.into()))
+        .into_iter()
+    );
+    disp.flush().unwrap();
 }
